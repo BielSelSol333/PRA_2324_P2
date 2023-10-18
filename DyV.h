88d608402c81a9879103ff866802246a8e85b74f
BielSelSol <bielselsol@gmail.com>
2023-10-18 20:42:02 +0200

    Implementado el template a el programa y añadido la busqueda inversa

--- DyV.h
+++ DyV.h
@@ -1,3 +1,29 @@
 #include <vector>
 
-int busquedaBinaria(int x, std::vector<int> v, int ini, int fin);
+template <typename T> int busquedaBinaria(T x, std::vector<int> v, int ini, int fin){
+  if(ini > fin){
+                return -1;
+        }
+        int medio = (ini + fin) / 2;
+        if(v[medio] == x){
+                return medio;
+        }else if(v[medio] > x){
+                return busquedaBinaria(x, v, ini, medio - 1);
+        }else{
+                return busquedaBinaria(x, v, medio + 1, fin);
+        }
+}
+
+template <typename T> int busquedaBinaria_Inv(T x, std::vector<int> v, int ini, int fin){
+  	if(ini > fin){
+                return -1;
+        }
+        int medio = (ini + fin) / 2;
+        if(v[medio] == x){
+                return medio;
+        }else if(v[medio] > x){
+                return busquedaBinaria_Inv(x, v, medio + 1, fin);
+        }else{
+                return busquedaBinaria_Inv(x, v, ini, medio - 1);
+        }
+}
--- testDyV.cpp
+++ testDyV.cpp
@@ -4,15 +4,21 @@
 #include "DyV.h"
 
 int main(){
-        std::vector<int> v{1, 3, 5, 7, 8};
-        int tam = v.size();
-	std::cout << tam << std::endl;
+        std::vector<int> v1{1, 3, 5, 7, 8};
+        int tam1 = v1.size();
 
-        int x, pos;
+	std::vector<int> v2{9, 7, 5, 3, 1};
+	int tam2 = v2.size();
+
+        int x, pos1, pos2;
         std::cout << "Inserta el elemento a buscar: " << std::endl;
         std::cin >> x;
 
-        pos = busquedaBinaria(x, v, 0, tam);
-	std::cout << "Posición: " << pos << std::endl;
+        pos1 = busquedaBinaria(x, v1, 0, tam1);
+	std::cout << "Posición: " << pos1 << std::endl;
+
+	pos2 = busquedaBinaria_Inv(x, v2, 0, tam2);
+        std::cout << "Posición invertida: " << pos2 << std::endl;
+
         return 0;
 }
